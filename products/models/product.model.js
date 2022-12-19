de2da2021c813f566e39e4efae7fb756f5906a7c
David-Santoryu <davsubiela@gmail.com>
2022-12-19 23:48:58 +0100

    Afegit PUT a totes els arxius

--- products/models/product.model.js
+++ products/models/product.model.js
@@ -18,3 +18,57 @@ const productsSchema = new mongoose.Schema(
 
 productsSchema.set("toJSON", { virtuals: false });
 const Products = mongoose.model("products", productsSchema, "products");
+
+exports.getProducts = () => {
+  return new Promise((resolve, reject) => {
+    Products.find({}).exec((error, result) => {
+      if (error) {
+        reject(error.message);
+        throw error.message;
+      }
+      if (result) {
+        resolve(result);
+      }
+    });
+  }).catch((error) => {
+    throw error.message;
+  });
+};
+
+exports.addProducts = (info) => {
+  try {
+    const product = new Products(info);
+
+    return product.save().catch((error) => error.message);
+  } catch (error) {
+    throw error.message;
+  }
+};
+
+exports.deleteProducts = (id) => {
+  return new Promise((resolve, reject) => {
+    Products.deleteOne({ _id: id }).exec((error, result) => {
+      if (error) {
+        reject(error.message);
+        throw error.message;
+      }
+      if (result.deletedCount) {
+        resolve(result);
+      } else {
+        resolve(false);
+      }
+    });
+  }).catch((error) => {
+    throw error.message;
+  });
+};
+
+exports.modifyProducts = (info) => {
+  try {
+    const product = new Products(info);
+
+    return product.save().catch((error) => error.message);
+  } catch (error) {
+    throw error.message;
+  }
+};
